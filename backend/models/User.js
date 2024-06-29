0c3bd3d040b585c204bbf02a90ac11bdd99e9584
Yavuzhan <99103025+yavuzhan66@users.noreply.github.com>
2024-06-29 22:09:48 +0200

    NEWWW

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.plugin(uniqueValidator);
 
-module.exports = User;
+module.exports = mongoose.model('User', userSchema);;
